04176ad975fcb098d631224744cfd9f4c5bcb6cd
Adam Kunicki <adam@streamsets.com>
2017-06-15 21:40:32 -0700

    Start roughing in scripting proc

--- src/main/kotlin/com/adamkunicki/streamsets/datacollector/stage/processor/kotlin/KotlinScriptingProcessor.kt
+++ src/main/kotlin/com/adamkunicki/streamsets/datacollector/stage/processor/kotlin/KotlinScriptingProcessor.kt
@@ -5,11 +5,22 @@ import com.streamsets.pipeline.api.Field
 import com.streamsets.pipeline.api.Processor
 import com.streamsets.pipeline.api.Stage
 import com.streamsets.pipeline.api.base.SingleLaneProcessor
+import mu.KLogging
+import javax.script.ScriptEngine
+import javax.script.ScriptEngineManager
 
 class KotlinScriptingProcessor : SingleLaneProcessor() {
+  var engine: ScriptEngine? = null
 
   override fun init(info: Stage.Info?, context: Processor.Context?): MutableList<Stage.ConfigIssue>? {
-    return super.init(info, context)
+    val issues = super.init(info, context)
+
+    // Initialize Script Engine
+    val factory = ScriptEngineManager().getEngineByExtension("kts").factory
+    engine = factory.scriptEngine
+    val bindings = engine?.createBindings()
+
+    return issues
   }
 
   override fun destroy() {
@@ -21,4 +32,6 @@ class KotlinScriptingProcessor : SingleLaneProcessor() {
       record.set("/test", Field.create("value"))
     }
   }
+
+  companion object: KLogging()
 }
